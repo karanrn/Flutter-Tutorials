8f634b2d5fcf77e47c6de799b6b9823538548d1b
Karan Nadagoudar <karan4080@gmail.com>
2020-03-03 07:02:07 +0530

    Added SharedPreferences to save loaded theme

--- sample_app/lib/theme_manager.dart
+++ sample_app/lib/theme_manager.dart
@@ -1,23 +1,42 @@
 import 'package:flutter/material.dart';
 import 'package:sample_app/app_themes.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class ThemeManager with ChangeNotifier {
- ThemeData _themeData;
- 
- /// Use this method on UI to get selected theme.
- ThemeData get themeData {
-   if (_themeData == null) {
-     _themeData = appThemeData[AppTheme.White];
-   }
-   return _themeData;
- }
+  ThemeData _themeData;
+  final String _kthemePreference = "theme_preference";
+  
+  ThemeManager(){
+    _loadTheme();
+  }
+  // Load saved theme
+  void _loadTheme(){
+    debugPrint("Entered loadTheme");
+    SharedPreferences.getInstance().then((prefs){
+      int preferedTheme = prefs.getInt(_kthemePreference) ?? 0;
+      _themeData = appThemeData[AppTheme.values[preferedTheme]];
 
- /// Sets theme and notifies listeners about change. 
- setTheme(AppTheme theme) async {
-   _themeData = appThemeData[theme];
+      // Notify listeners
+      notifyListeners();
+    });
+  }
+  // Use this method on UI to get selected theme.
+  ThemeData get themeData {
+    if (_themeData == null) {
+      _themeData = appThemeData[AppTheme.White];
+    }
+    return _themeData;
+  }
 
-   // Here we notify listeners that theme changed 
-   // so UI have to be rebuild
-   notifyListeners();
- }
+  // Sets theme and notifies listeners about change. 
+  setTheme(AppTheme theme) async {
+    _themeData = appThemeData[theme];
+
+    // Save the selected theme into Sharedpreferences
+    var prefs = await SharedPreferences.getInstance();
+    prefs.setInt(_kthemePreference, AppTheme.values.indexOf(theme));
+    // Here we notify listeners that theme changed 
+    // so UI have to be rebuild
+    notifyListeners();
+  }
 }
